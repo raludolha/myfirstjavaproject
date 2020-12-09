3386e62a69a56493daa3bb864afdaf5b2300a884
Raluca Dolha <ralucafalcusan@yahoo.com>
2020-12-09 21:48:23 +0200

    Introducere in Java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello Ralu");
+        // write your code here
+        System.out.println("Hello");
+        System.out.println("Ralu");
+        System.out.println(1+2);
+        int x = 10;
+        int y = 3;
+        int z = x+y;
+        int zz = 10+3;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(zz);
+        System.out.println(++z);
+        System.out.println((float)x/y);
+        System.out.println(-5+8*6);
+        System.out.println((55 + 9) % 9);
+        System.out.println(20 + (float)(-3 * 5) / 8);
+        System.out.println(5 + (float)15 / 3 * 2 - 8 % 3);
     }
 }
